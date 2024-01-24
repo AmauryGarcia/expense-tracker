50116d9883ab369dd6f8a947a065932fc805239d
amaurygarcia <amaurygp9@gmail.com>
2024-01-23 22:39:21 -0500

    upload info

--- src/components/transactions/TransactionForm.jsx
+++ src/components/transactions/TransactionForm.jsx
@@ -36,9 +36,37 @@ function TransactionForm() {
           className="bg-zinc-600 text-white px-3 py-2 rounded-lg block mb-2 w-full"
           value={amount}
         />
-        <button className="bg-indigo-700 text-white px-3 py-2 
-        rounded-lg block mb-2 w-full">Add Transaction</button>
+        <button
+          className="bg-indigo-700 text-white px-3 py-2 
+        rounded-lg block mb-2 w-full"
+        >
+          Add Transaction
+        </button>
       </form>
+
+      <div class="flex w-full my-6 max-w-sm overflow-hidden bg-zinc-600 rounded-lg shadow-md dark:bg-gray-800">
+        <div class="flex items-center justify-center w-12 bg-blue-500">
+          <svg
+            class="w-6 h-6 text-white fill-current"
+            viewBox="0 0 40 40"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M20 3.33331C10.8 3.33331 3.33337 10.8 3.33337 20C3.33337 29.2 10.8 36.6666 20 36.6666C29.2 36.6666 36.6667 29.2 36.6667 20C36.6667 10.8 29.2 3.33331 20 3.33331ZM21.6667 28.3333H18.3334V25H21.6667V28.3333ZM21.6667 21.6666H18.3334V11.6666H21.6667V21.6666Z" />
+          </svg>
+        </div>
+
+        <div class="px-4 py-2 -mx-3">
+          <div class="mx-3">
+            <span class="font-semibold text-blue-500 dark:text-blue-400">
+              Info
+            </span>
+            <p class="text-sm text-white-600 dark:text-gray-200">
+              Poner los números en negativo cuando hagan una compra: "-5000"
+            </p>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
